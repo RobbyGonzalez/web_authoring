39b88fa649ee99bf85a9ebb827effed29df183f5
RobbyGonzalez <robbygonzalez@gmail.com>
2023-05-09 17:38:10 -0500

    finalizing about us page

--- js/modal.js
+++ js/modal.js
@@ -5,32 +5,38 @@ document.addEventListener("DOMContentLoaded", function () {
     figCaption.forEach(figCaption => {
         figCaption.style.display = "none";
     });
-    
+
 
     // Get the modal
-    var modal = document.getElementById("myModal");
+    var modal = document.querySelectorAll(".modal");
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    //need to make a new function to loop through the node list.
+    // Get the image and insert it inside the modal - use its caption text as a caption
+    var img = document.querySelectorAll(".img-thumbnail");
+    // var modalImg = document.querySelectorAll(".modal-content");
+    var video = document.querySelectorAll(".video");
+    var captionText = document.querySelectorAll(".caption");
+    img.forEach(img => {
+        img.addEventListener("click", (event) => {
+            var index = event.target.id;
+            // var sourceLink = event.target.src
 
-    var img = document.getElementById("myImg");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function () {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = figCaption.innerHTML;
-        document.querySelector("header").classList.remove("sticky-top");
-    }
+            modal[index].style.display = "block";
+            // modalImg[index].src = sourceLink.substring(0, sourceLink.lastIndexOf(".")) + ".mp4";
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+            //add auto play to video?
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
+            captionText[index].innerHTML = figCaption[index].innerHTML;
+            document.querySelector("header").classList.remove("sticky-top");
 
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[index];
 
-
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal[index].style.display = "none";
+                video[index].pause();
+                video[index].currentTime = 0;
+            }
+        }); //end click
+    }); //end forEach img event listener
 }); //end wait load event listerner
